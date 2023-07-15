33e2b9a8e347d5f769d2a4add366e27a45d15e4b
vikas <vikaspatil212222@gmail.com>
2023-07-15 11:14:59 +0530

    openai

--- Routes/router.js
+++ Routes/router.js
@@ -8,12 +8,28 @@ dotenv.config();
 import { UserInfo } from "./../Modals/modal.js";
 const otpStore = new Map();
 
+
+// setup for clarifai api
+import { ClarifaiStub, grpc } from "clarifai-nodejs-grpc";
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", `Key ${process.env.CLARIFAI_KEY}`);
+
+// setup for openai api
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const router = express.Router();
 
 
 
 const authenticateToken = (req, res, next) => {
-    
+
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
@@ -23,7 +39,7 @@ const authenticateToken = (req, res, next) => {
 
     jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
         if (err) {
-            return res.status(403).json({Error:err});
+            return res.status(403).json({ Error: err });
         }
         req.user = decoded;
         next();
@@ -107,7 +123,7 @@ router.post("/authorization-v", async (req, res) => {
             }
         }
 
-        const token = jwt.sign(jwtOptions, process.env.TOKEN_SECRET_KEY, { expiresIn: 60*60*24 });
+        const token = jwt.sign(jwtOptions, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 24 });
 
         res.status(200).json({ status: "ok", token: token });
     } catch (error) {
@@ -116,6 +132,101 @@ router.post("/authorization-v", async (req, res) => {
 });
 
 
+
+
+
+// predict image concepts(labels)
+
+const predictImage = (inputs) => {
+    return new Promise((resolve, reject) => {
+        stub.PostModelOutputs(
+            {
+                user_app_id: {
+                    "user_id": "clarifai",
+                    "app_id": "main"
+                },
+                model_id: "general-image-recognition",
+                inputs: inputs
+            },
+            metadata,
+            (err, response) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                if (response.status.code !== 10000) {
+                    reject("Received failed status: " + response.status.description + "\n" + response.status.details);
+                    return;
+                }
+
+                const output = response.outputs[0].data.concepts;
+
+                const filteredOutput = output.filter(each => {
+                    if (each.value >= 0.8) {
+                        return each;
+                    }
+                })
+                const concepts = filteredOutput.map(each => {
+                    return each.name;
+
+                })
+
+                resolve(concepts)
+            }
+        );
+
+    })
+}
+
+
+// generate a caption from labels
+const openaiCaption = (labelArr) => {
+    return new Promise((resolve, reject) => {
+
+        openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${labelArr} generate an elegant and classic instagram caption based on labels and use hashtags not more than 3`,
+            max_tokens: 300,
+            temperature: 0.1,
+         }).then((response)=>{
+           
+           resolve(response.data.choices[0].text);
+         }).catch(err=>{
+          reject(err)
+         })
+   })
+
+
+}
+
+// prompt 
+
+router.post('/prompt', authenticateToken, async (req, res) => {
+    try {
+        const { imageURL } = req.body;
+
+        const inputs = [
+            {
+                data: {
+                    image: {
+                        url: imageURL
+                    }
+                }
+            }
+        ]
+
+        const resultLabels = await predictImage(inputs);
+        const result = await openaiCaption(resultLabels);
+
+        return res.send({
+            result
+        })
+
+    } catch (error) {
+        res.status(400).send({ Error: error.message })
+    }
+})
 
 
 export default router;
